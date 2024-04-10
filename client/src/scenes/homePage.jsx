94332d62e773e7421e30e4855d7c0117fa05ae60
Johnson179 <j179988143412@gmail.com>
2024-04-10 13:56:21 +0800

    Finish all the implementation

--- client/src/scenes/homePage.jsx
+++ client/src/scenes/homePage.jsx
@@ -61,7 +61,7 @@ const HomePage = () => {
         })
         .then((data) =>{
             setUserPosts(data.userPosts);
-            setFriendsPosts(data.friendsPosts);
+            setFriendsPosts(data.friendsPosts.reverse());
             setIsLoading(false);
         })
         .catch((err) => {
